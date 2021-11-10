e0bc09a1c1f5711c3c277257a3a76c727782b9f2
rollingman1 <bloodtype0@khu.ac.kr>
2021-11-10 14:09:30 +0900

    test: 음성 파어리 테스트해보기

--- scripts/0_inference.sh
+++ scripts/0_inference.sh
@@ -11,7 +11,7 @@ wget -O $CHECKPOINT_PATH "https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.
 python3 pytorch/inference.py audio_tagging \
     --model_type=$MODEL_TYPE \
     --checkpoint_path=$CHECKPOINT_PATH \
-    --audio_path="resources/R9_ZSCveAHg_7s.wav" \
+    --audio_path="/home/pepeotalk/Downloads/RAP-COVER_-흰둥이-성우-정유미의-_머쉬베놈-커버-랩_-짖었..-아니-찢었다_.wav" \
     --cuda
 
 # ------ Inference sound event detection result with pretrained model. ------
@@ -25,6 +25,6 @@ wget -O $CHECKPOINT_PATH "https://zenodo.org/record/3987831/files/Cnn14_Decision
 python3 pytorch/inference.py sound_event_detection \
     --model_type=$MODEL_TYPE \
     --checkpoint_path=$CHECKPOINT_PATH \
-    --audio_path="resources/R9_ZSCveAHg_7s.wav" \
+    --audio_path="/home/pepeotalk/Downloads/RAP-COVER_-흰둥이-성우-정유미의-_머쉬베놈-커버-랩_-짖었..-아니-찢었다_.wav" \
     --cuda
     
